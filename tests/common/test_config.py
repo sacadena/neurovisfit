030addab1b4ed43391e134ceba3971bf3798d47f
Santiago Cadena <scadena@lyft.com>
2023-06-15 22:26:49 +0200

    add tests loader

--- tests/common/test_config.py
+++ tests/common/test_config.py
@@ -5,49 +5,7 @@ import pytest
 from cookie_test.common.config import TomlConfig
 
 
-@pytest.fixture
-def mock_toml_config(tmp_path):
-    # Create a temporary file with test data
-    toml_data = """
-    [some_data_name]
-    dataset = "some_data_name"
-    batch_size = 32
-    validation_fraction = 0.2
-    seed = 42
-    include_prev_image = true
-    include_trial_id = false
-
-    [some_data_name.data]
-    path = "data/v1_data"
-    exclude = []
-
-    [some_data_name.image_transform]
-    subsample = 1
-    crop = [96, 96, 96, 96]
-    scale = 1.0
-
-    [some_data_name.process_time_bins]
-    bin_duration_ms = 10
-    num_bins = 12
-    offset_first_bin_ms = 40
-    window_range_ms = [40, 160]
-    agg_operation = "mean"
-
-    [some_data_name.train_with_fraction_of_images]
-    fraction = 1.0
-    randomize_selection = false
-    """
-    file_path = tmp_path / "test_config.toml"
-    with open(file_path, "w") as f:
-        f.write(toml_data)
-
-    # Create an instance of TomlConfig for testing
-    toml_config = TomlConfig(file_path)
-
-    return toml_config
-
-
-def test_toml_config_get_dict(mock_toml_config):
+def test_toml_config_get_dict(mock_toml_config, mock_data_path):
     database_config = mock_toml_config.get_dict("some_data_name")
     assert isinstance(database_config, dict)
     assert database_config["dataset"] == "some_data_name"
@@ -56,16 +14,16 @@ def test_toml_config_get_dict(mock_toml_config):
     assert database_config["seed"] == 42
     assert database_config["include_prev_image"] is True
     assert database_config["include_trial_id"] is False
-    assert database_config["data"]["path"] == "data/v1_data"
+    assert database_config["data"]["path"] == str(mock_data_path)
     assert database_config["data"]["exclude"] == []
     assert database_config["image_transform"]["subsample"] == 1
-    assert database_config["image_transform"]["crop"] == [96, 96, 96, 96]
+    assert database_config["image_transform"]["crop"] == [2, 2, 2, 2]
     assert database_config["image_transform"]["scale"] == 1.0
     assert database_config["process_time_bins"] == {
         "bin_duration_ms": 10,
         "num_bins": 12,
         "offset_first_bin_ms": 40,
-        "window_range_ms": [40, 160],
+        "window_range_ms": [40, 70],
         "agg_operation": "mean",
     }
     assert database_config["train_with_fraction_of_images"] == {
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytest
 from PIL import Image
 
+from cookie_test.common.config import TomlConfig
 from cookie_test.data.cache import ImageCache
 
 
@@ -72,7 +73,49 @@ def mock_data_path(tmpdir):
             with (session_path / "responses.csv").open("w") as f:
                 writer = csv.writer(f)
                 writer.writerow(["responses", "image_id"])
-                writer.writerow(["[0, 1, 0]", "1"])
+                writer.writerow(["[[0, 1, 0],[1, 1, 0]]", "1"])
             with (session_path / "meta_data.json").open("w") as f:
                 json.dump({"subject_id": "001", "session_id": "123"}, f)
     return temp_dir
+
+
+@pytest.fixture
+def mock_toml_config(tmp_path, mock_data_path):
+    # Create a temporary file with test data
+    toml_data = f"""
+    [some_data_name]
+    dataset = "some_data_name"
+    batch_size = 32
+    validation_fraction = 0.2
+    seed = 42
+    include_prev_image = true
+    include_trial_id = false
+
+    [some_data_name.data]
+    path = "{str(mock_data_path)}"
+    exclude = []
+
+    [some_data_name.image_transform]
+    subsample = 1
+    crop = [2, 2, 2, 2]
+    scale = 1.0
+
+    [some_data_name.process_time_bins]
+    bin_duration_ms = 10
+    num_bins = 12
+    offset_first_bin_ms = 40
+    window_range_ms = [40, 70]
+    agg_operation = "mean"
+
+    [some_data_name.train_with_fraction_of_images]
+    fraction = 1.0
+    randomize_selection = false
+    """
+    file_path = tmp_path / "test_config.toml"
+    with open(file_path, "w") as f:
+        f.write(toml_data)
+
+    # Create an instance of TomlConfig for testing
+    toml_config = TomlConfig(file_path)
+
+    return toml_config
