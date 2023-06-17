b31d49f2f1eb22e7c9cc66a6d506a3d294d32d54
Santiago Cadena <scadena@lyft.com>
2023-06-17 14:00:59 +0200

    improve data dimensions tests

--- tests/common/test_dimensions.py
+++ tests/common/test_dimensions.py
@@ -1,11 +1,56 @@
+from collections import namedtuple
+from typing import Dict
+from typing import Tuple
+
+import pytest
 import torch
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 
 from neurovisfit.common.dimensions import get_dims_for_loader_dict
 from neurovisfit.common.dimensions import get_io_dims
 
 
-def test_get_io_dims():
+@pytest.fixture
+def mock_named_tuple_dataset():
+    class MockDataset(Dataset):
+        def __init__(self) -> None:
+            self.inputs = torch.tensor([1, 2, 3, 4])
+            self.responses = torch.tensor([5, 6, 7, 8])
+
+        def __len__(self) -> int:
+            return len(self.inputs)
+
+        def __getitem__(self, key: int) -> Tuple[torch.Tensor, torch.Tensor]:
+            SomeData = namedtuple("SomeData", ["inputs", "responses"])
+            return SomeData(
+                inputs=self.inputs[key],
+                responses=self.responses[key],
+            )
+
+    return MockDataset()
+
+
+@pytest.fixture
+def mock_dict_dataset():
+    class MockDataset(Dataset):
+        def __init__(self) -> None:
+            self.inputs = torch.tensor([1, 2, 3, 4])
+            self.responses = torch.tensor([5, 6, 7, 8])
+
+        def __len__(self) -> int:
+            return len(self.inputs)
+
+        def __getitem__(self, key: int) -> Dict[str, torch.Tensor]:
+            return {
+                "inputs": self.inputs[key],
+                "responses": self.responses[key],
+            }
+
+    return MockDataset()
+
+
+def test_get_io_dims(mock_named_tuple_dataset, mock_dict_dataset):
     data = torch.tensor([1, 2, 3, 4])  # Example data
     batch_size = 2
     data_loader = DataLoader(data, batch_size=batch_size)
@@ -14,6 +59,14 @@ def test_get_io_dims():
 
     assert isinstance(result, tuple)
     assert len(result) == 2
+
+    data_loader = DataLoader(mock_named_tuple_dataset, batch_size=batch_size)
+    result = get_io_dims(data_loader)
+    assert isinstance(result, dict)
+
+    data_loader = DataLoader(mock_dict_dataset, batch_size=batch_size)
+    result = get_io_dims(data_loader)
+    assert isinstance(result, dict)
 
 
 def test_get_dims_for_loader_dict():
