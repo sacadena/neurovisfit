a794afa5a7913c2a7946421ff2c384e0fa61d4f9
Santiago Cadena <sa.cadena721@gmail.com>
2024-04-23 18:48:24 +0200

    Added metrics

---
+++ neurovisfit/evaluation/metrics.py
@@ -0,0 +1,136 @@
+import warnings
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Union
+
+import numpy as np
+import torch
+from neuralpredictors.measures import corr
+
+from ..models.model import Model
+from .predictions import compute_model_predictions_for_repeated_input_loaders
+
+
+def compute_feve_from_target_and_outputs(
+    targets: Union[torch.Tensor, np.ndarray, List[np.ndarray]],
+    outputs: Union[torch.Tensor, np.ndarray, List[np.ndarray]],
+    return_fraction_explainable_var: bool = False,
+) -> Union[Tuple[np.ndarray, np.ndarray], np.ndarray]:
+    """
+
+    Args:
+        targets (list): Neuronal responses (ground truth) to image repeats.
+            Dimensions: [num_images] np.array(num_reaps, num_neurons)
+        outputs (list): Model predictions to the repeated images, with an identical shape as the targets
+        return_fraction_explainable_var (bool): returns the fraction of explainable variance per neuron
+            if set to True
+
+    Returns:
+        FEVe (np.array): the fraction of explainable variance explained per neuron
+        --- optional: FEV (np.array): the fraction
+
+    """
+    image_variance = []
+    predictions_variance = []
+
+    for i, _ in enumerate(targets):
+        predictions_variance.append((targets[i] - outputs[i]) ** 2)
+        image_variance.append(np.var(targets[i], axis=0, ddof=1))
+
+    predictions_variance = np.vstack(predictions_variance)
+    image_variance = np.vstack(image_variance)
+
+    total_variance = np.var(np.vstack(targets), axis=0, ddof=1)
+    noise_variance = np.mean(image_variance, axis=0)
+    fraction_explainable_variance = (total_variance - noise_variance) / total_variance
+
+    predictions_variance_averaged = np.mean(predictions_variance, axis=0)
+    fraction_explainable_variance_explained = 1 - (predictions_variance_averaged - noise_variance) / (
+        total_variance - noise_variance
+    )
+    if return_fraction_explainable_var:
+        return fraction_explainable_variance, fraction_explainable_variance_explained
+    return fraction_explainable_variance_explained
+
+
+def compute_correlation_to_average_signal(
+    model: Model,
+    dataloaders: Dict[str, Any],
+    device: str = "cpu",
+    as_dict: bool = False,
+    per_neuron: bool = True,
+    min_fraction_explainable_variance: Optional[float] = None,
+) -> Union[Dict[str, Any], np.ndarray, float]:
+    if "test" in dataloaders:
+        dataloaders = dataloaders["test"]
+
+    correlations = {}
+    for data_key, dataloader in dataloaders.items():
+        # Get targets and outputs
+        targets, outputs = compute_model_predictions_for_repeated_input_loaders(
+            dataloader=dataloader,
+            model=model,
+            data_key=data_key,
+            device=device,
+            broadcast_to_target=True,
+        )
+
+        # Get correlation to avg. targets
+        outputs_avg = np.array([out.mean(axis=0) for out in outputs])
+        targets_avg = np.array([t.mean(axis=0) for t in targets])
+        raw_correlations = corr(targets_avg, outputs_avg, axis=0)
+
+        if min_fraction_explainable_variance is None:
+            correlations[data_key] = raw_correlations
+        else:
+            exp_var_frac, _ = compute_feve_from_target_and_outputs(
+                targets=targets,
+                outputs=outputs,
+                return_fraction_explainable_var=True,
+            )
+            correlations[data_key] = raw_correlations[exp_var_frac > min_fraction_explainable_variance]
+
+        # Check for nans
+        if np.any(np.isnan(correlations[data_key])):
+            warnings.warn("{}% NaNs , NaNs will be set to Zero.".format(np.isnan(correlations[data_key]).mean() * 100))
+        correlations[data_key][np.isnan(correlations[data_key])] = 0
+
+    if not as_dict:
+        correlations_arr = np.hstack(list(correlations.values()))
+        correlations = correlations_arr if per_neuron else np.mean(correlations_arr)
+
+    return correlations
+
+
+def compute_feve(
+    model: Model,
+    dataloaders: Dict[str, Any],
+    as_dict: bool = False,
+    device: str = "cuda",
+    per_neuron: bool = True,
+    min_fraction_explainable_variance: Optional[float] = None,
+) -> Union[Dict[str, Any], np.ndarray, float]:
+    dataloaders = dataloaders["test"] if "test" in dataloaders else dataloaders
+    feve = {}
+    for data_key, dataloader in dataloaders.items():
+        targets, outputs = compute_model_predictions_for_repeated_input_loaders(
+            model=model,
+            dataloader=dataloader,
+            data_key=data_key,
+            device=device,
+            broadcast_to_target=True,
+        )
+        if min_fraction_explainable_variance is None:
+            feve[data_key] = compute_feve_from_target_and_outputs(targets=targets, outputs=outputs)
+        else:
+            fev, feve = compute_feve_from_target_and_outputs(
+                targets=targets, outputs=outputs, return_fraction_explainable_var=True
+            )
+            feve[data_key] = feve[fev > min_fraction_explainable_variance]
+
+    if not as_dict:
+        feve = np.hstack([v for v in feve.values()]) if per_neuron else np.mean(np.hstack([v for v in feve.values()]))
+    return feve
---
+++ neurovisfit/evaluation/predictions.py
@@ -0,0 +1,81 @@
+from typing import Optional
+from typing import Tuple
+
+import numpy as np
+import torch
+from neuralpredictors.training import device_state
+from neuralpredictors.training import eval_state
+from torch.utils.data import DataLoader
+
+from ..models.model import Model
+
+
+def compute_model_predictions_for_repeated_input_loaders(
+    model: Model,
+    dataloader: DataLoader,
+    data_key: str,
+    device: str = "cuda",
+    broadcast_to_target: bool = False,
+    repeat_channel_dim: Optional[int] = None,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Computes model predictions for a dataloader that yields batches with identical inputs along the first dimension.
+    Unique inputs will be forwarded only once through the model
+    Returns:
+        target: ground truth, i.e. neuronal firing rates of the neurons as a list: [num_images][num_reaps, num_neurons]
+        output: responses as predicted by the network for the unique images. If broadcast_to_target, returns repeated
+                outputs of shape [num_images][num_reaps, num_neurons] else (default) returns unique outputs of shape
+                [num_images, num_neurons]
+    """
+    target, output = [], []
+    unique_images = torch.empty(0).to(device)
+    for batch in dataloader:
+        images, responses = batch[:2]
+
+        if len(images.shape) == 5:
+            images = images.squeeze(dim=0)
+            responses = responses.squeeze(dim=0)
+
+        first_image = images[0, :1, ...]
+        for im in images:
+            if not torch.equal(im, first_image):
+                raise ValueError("expected all images in the batch to be equal")
+
+        unique_images = torch.cat(
+            tensors=(unique_images, images[0:1, ...].to(device)),
+            dim=0,
+        )
+        target.append(responses.detach().cpu().numpy())
+
+        if images.shape[0] > 1:
+            with eval_state(model):
+                with device_state(model, device=device):
+                    output.append(
+                        model(
+                            images.to(device),
+                            data_key=data_key,
+                            **batch._asdict(),
+                            repeat_channel_dim=repeat_channel_dim,
+                        )
+                        .detach()
+                        .cpu()
+                        .numpy()
+                    )
+
+    # Forward unique images once
+    if len(output) == 0:
+        with eval_state(model):
+            with device_state(model, device):
+                output = (
+                    model(unique_images.to(device), data_key=data_key, repeat_channel_dim=repeat_channel_dim)
+                    .detach()
+                    .cpu()
+                    .numpy()
+                )
+
+    if broadcast_to_target:
+        output = [np.broadcast_to(x, target[idx].shape) for idx, x in enumerate(output)]
+
+    target = np.asarray(target)
+
+    return target, output
